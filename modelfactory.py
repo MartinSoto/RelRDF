61cf4405e505db6816673075ee1aea174e0691b9
Martin Soto <martin.soto@iese.fraunhofer.de>
2006-03-02 15:19:41 +0000

    New factory for high-level RDF models * modelfactory.py: New module containing factory methods for creating   abstract models that are able to run queries. The query results are   also encapsulated in an object that abstracts the details of retrieving   teh actual results. This class should serve as a framework to   incorporate other database mappings or database access methods.

---
+++ modelfactory.py
@@ -0,0 +1,90 @@
+import gettext
+gettext.install('relrdf')
+
+import serql
+from sqlmap import map
+from sqlmap import generate
+
+
+def getQueryParser(queryLanguage, prefixes=None):
+    queryLanguageNorm = queryLanguage.lower()
+    if queryLanguageNorm == 'serql':
+        return serql.Parser(prefixes)
+    else:
+        assert False, "invalid query language '%s'" % queryLanguage
+
+
+class SqlBasedResults(object):
+    """A query that gets processed by translating it to SQL."""
+
+    __slots__ = ('cursor',
+                 'columnNames')
+
+    def __init__(self, connection, columnNames, queryString):
+        self.cursor = connection.cursor()
+        self.columnNames = columnNames
+        self.cursor.execute(queryString)
+
+    def iterAll(self):
+        row = self.cursor.fetchone()
+        while row is not None:
+            yield row
+            row = self.cursor.fetchone()
+
+
+class SqlMappedModel(object):
+    """An RDF model defined through a certain mapping from relational
+    expressions to SQL."""
+
+    __slots__ = ('connection',
+                 'mapper',
+                 'prefixes')
+
+    def __init__(self, connection, mapper, prefixes=None):
+        self.connection = connection
+        self.mapper = mapper
+        self.prefixes = prefixes
+
+    def query(self, queryLanguage, queryText, fileName=_("<unknown>")):
+        parser = getQueryParser(queryLanguage, self.prefixes)
+        expr = parser.parse(queryText, fileName)
+        columnNames = expr.columnNames
+        expr = self.mapper.mapExpression(expr)
+        return SqlBasedResults(self.connection, columnNames,
+                               generate.generate(expr))
+
+
+def getModel(modelType, connection, prefixes=None, **kwargs):
+    modelTypeNorm = modelType.lower()
+    if modelTypeNorm == 'singleversion':
+        mapper = map.VersionMapper(kwargs['versionId'])
+    elif modelTypeNorm == 'multiversion':
+        mapper = map.MultiVersionMapper(kwargs['versionUri'])
+    else:
+        assert False, "invalid model type '%s'" % modelType
+
+    return SqlMappedModel(connection, mapper, prefixes)
+ 
+
+if __name__ == '__main__':
+
+    import sys
+    import MySQLdb
+
+    prefixes = {
+        'ex': 'http://example.com/model#'
+        }
+
+    connection = MySQLdb.connect(host='localhost', db=sys.argv[1],
+                                 read_default_group='client')
+
+    kwargs = {}
+    for arg in sys.argv[3:]:
+        key, value = arg.split('=')
+        kwargs[key] = value
+
+    model = getModel(sys.argv[2], connection, prefixes, **kwargs)
+    result = model.query('SerQL', sys.stdin)
+    print result.columnNames
+    for row in result.iterAll():
+        print row

16e2bb5f1e7bc0b14193d2ac8ed036815c682042
Martin Soto <martin.soto@iese.fraunhofer.de>
2006-04-03 12:04:03 +0000

    Add ASG namespace for testing * browser/prefixes.py (namespaces): Add ASG namespace for testing.

--- browser/prefixes.py
+++ browser/prefixes.py
@@ -1,11 +1,14 @@
 namespaces = {
     'rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
     'rdfs': 'http://www.w3.org/2000/01/rdf-schema#',
+
     'vmxt': 'http://www.v-modell.iabg.de/Schema#',
     'inst': 'http://www.kbst.bund.de/V-Modell-XT.xml#',
 
     'modale': 'http://www.modale.de/Beispiel#',
-    'kuka': 'http://www.kuka.de/Modale#'}
+    'kuka': 'http://www.kuka.de/Modale#',
+
+    'asg': 'https://asg-platform.org/cgi-bin/twiki/viewauth/Internal/ASGDevelopmentProcess#'}
 
 
 def shortenUri(uri):

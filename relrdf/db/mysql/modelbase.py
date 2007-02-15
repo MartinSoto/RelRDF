32d336e40e74a47c5d0b158a5e39b3962c252166
Martin Soto <soto@iese.fhg.de>
2007-02-15 17:03:46 +0100

    Limit the time used by the MySQL optimizer
    * relrdf/db/mysql/modelbase.py (ModelBase.__init__): Document MySQL
      connection options. Add an option to reduce the maximum time used by
      the query optimizer. This is necessary for complex queries to run
      properly.

--- relrdf/db/mysql/modelbase.py
+++ relrdf/db/mysql/modelbase.py
@@ -16,7 +16,16 @@ class ModelBase(object):
         # it doesn't decode the results accordingly. We tell the
         # database to send results back in UTF-8.
         cursor = self.connection.cursor()
+
+        # Set the result character set to unicode. Changing it
+        # generally using "SET names" causes problems with MySQLdb and
+        # queries containing non-ASCII characters.
         cursor.execute('SET character_set_results = utf8')
+
+        # This is necessary for complex queries to run at all. Due to
+        # the large the number of joins, queries with more than about
+        # 10 patterns take a very long time to optimize.
+        cursor.execute('SET optimizer_search_depth = 0')
         cursor.close()
 
         # We have only one database schema at this time.

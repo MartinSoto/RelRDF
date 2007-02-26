2e00ce0c7ef127548d83f90b7d6f083f9511a06d
Martin Soto <soto@iese.fhg.de>
2007-02-26 17:21:24 +0100

    Literals inherit from unicode (again)
    * relrdf/expression/literal.py (Literal): Inherit (again) from
      unicode. Implement the old 'value' attribute as a property.

--- relrdf/expression/literal.py
+++ relrdf/expression/literal.py
@@ -1,48 +1,51 @@
 from relrdf.commonns import xsd
 
 
-class Literal(object):
-    __slots__ = ('value',
-                 'typeUri',
+class Literal(unicode):
+    __slots__ = ('typeUri',
                  'lang')
 
-    def __init__(self, value, lang=None, typeUri=None):
+    def __new__(cls, value, lang=None, typeUri=None):
         if lang is not None:
-            self.lang = lang
-            self.typeUri = None
-            self.value = value
+            typeUri = None
         elif typeUri is not None:
-            self.lang = None
-            self.typeUri = typeUri
-
-            # Check if a value conversion is necessary, and perform it.
-            if typeUri == xsd.boolean:
-                self.value = bool(value)
-            elif typeUri == xsd.integer:
-                self.value = int(value)
-            elif typeUri == xsd.decimal or typeUri == xsd.double:
-                self.value = float(value)
-            else:
-                self.value = str(value)
+            lang = None
         else:
-            self.lang = None
+            lang = None
 
             # Try to determine an appropriate type URI.
             if value is False or value is True:
-                self.typeUri = xsd.boolean
+                typeUri = xsd.boolean
             elif isinstance(value, int) or isinstance(value, long):
-                self.typeUri = xsd.integer
+                typeUri = xsd.integer
             elif isinstance(value, float):
-                self.typeUri = xsd.double
+                typeUri = xsd.double
             else:
-                self.typeUri = None
+                typeUri = None
 
-            self.value = value
+        literal = super(Literal, cls).__new__(cls, value)
+        literal.lang = lang
+        literal.typeUri = typeUri
+
+        return literal
+
+    def getValue(self):
+        """Return a Python value for the literal."""
+        # Check if a value conversion is necessary, and perform it.
+        if self.typeUri == xsd.boolean:
+            return bool(self)
+        elif self.typeUri == xsd.integer:
+            return int(self)
+        elif self.typeUri == xsd.decimal or self.typeUri == xsd.double:
+            return float(self)
+        else:
+            return self
+
+    value = property(getValue)
 
     def toXsd(self):
         """Return the literal's value in standard XSD representation."""
-        # FIXME: Check that this is correct in all practical cases.
-        return str(self.value)
+        return self
 
     def getCanonical(self):
         """Return the canonical string representation of the literal."""
@@ -52,9 +55,3 @@ class Literal(object):
             return '"%s"^^<%s>' % (self, self.typeUri)
         else:
             return '"%s"@%s' % (self, self.lang)
-
-    def __str__(self):
-        return str(self.value)
-
-    def __unicode__(self):
-        return unicode(self.value)

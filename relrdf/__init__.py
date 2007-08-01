91e8af5eb8993642ddc3b006e934fab520b70a85
Martin Soto <soto@iese.fhg.de>
2007-08-01 17:38:13 +0200

    Clean up main relrdf namespace
    * relrdf/__init__.py: Add new imports and clean up existing ones so that
      clients don't have to dig into subpackages when using the library.

--- relrdf/__init__.py
+++ relrdf/__init__.py
@@ -1,7 +1,21 @@
 import gettext
 gettext.install('relrdf')
 
-from error import *
-from results import *
+# Put public API elements in this module:
 
+# Error classes.
+from error import *
+
+# Result types.
+from results import RESULTS_COLUMNS, RESULTS_STMTS, RESULTS_MODIF
+
+# The basic building blocks of RDF.
+from relrdf.expression.uri import Uri, Namespace
+from relrdf.expression.literal import Literal
+
+# Common namespaces.
+import commonns as ns
+
+# Factory function to create model bases. Indirectly, this gives access
+# to queryable model objects.
 from modelbasefactory import getModelBase

1fcaa260320710b785c84411335f792b8a3beb00
Peter Wortmann <p_wortma@informatik.uni-kl.de>
2008-04-07 23:13:40 +0200

    First commit of Postgres database wrapper.
    Modified copy of the MySQL code. Supports two experimental database schemas, optimization is still pretty tricky.
    
    Known bugs:
    * Does not support the same Cast syntax as MySQL, so this will produce an error without changes
    * Will run pretty slow with MapValue flattening

---
+++ relrdf/db/postgres/basicsinks.py
@@ -0,0 +1,277 @@
+from relrdf.error import InstantiationError
+from relrdf.expression import uri, blanknode, literal
+from relrdf import commonns
+
+class SingleVersionRdfSink(object):
+    """An RDF sink that sends all tuples to a single model version in
+    the database."""
+
+    __slots__ = ('connection',
+                 'cursor',
+                 'versionId',
+                 'verbose',
+
+                 'pendingRows')
+
+    # Maximum number of rows per insert query.
+    ROWS_PER_QUERY = 10000
+
+    def __init__(self, connection, versionId, verbose=False):
+        self.connection = connection
+        self.versionId = versionId
+        self.verbose = verbose
+
+        self.pendingRows = []
+
+        self.cursor = self.connection.cursor()
+
+        # FIXME: A bug/limitation in MySQL causes an automatic commit
+        # whenever a table is dropped. To work around it, we use a
+        # different table for each required width and reuse them as
+        # needed. See basicquery.SingleVersionMapper.
+
+        # Create (if it doesn't exist already) a temporary table to
+        # hold the results. We create the hash column but don't fill
+        # it, since method 'close' issues an update to do this at
+        # server side.
+        self.cursor.execute(
+            """
+            CREATE TEMPORARY TABLE statements_temp1 (
+              subject_text text NOT NULL,
+              predicate_text text NOT NULL,
+              object_type text NOT NULL,
+              object_text text NOT NULL
+            ) ON COMMIT DROP;
+            """)
+        
+    def triple(self, subject, pred, object):
+        if isinstance(object, uri.Uri):
+            objectType = commonns.rdfs.Resource
+        elif isinstance(object, blanknode.BlankNode):
+            objectType = '<BLANKNODE>'
+        elif isinstance(object, literal.Literal):
+            if object.typeUri is None:
+                objectType = commonns.rdfs.Literal
+            else:
+                objectType = object.typeUri
+        else:
+            assert False, "Unexpected object type '%d'" \
+                   % object.__class__.__name__
+
+        self.pendingRows.append((unicode(subject).encode('utf-8'),
+                                 unicode(pred).encode('utf-8'),
+                                 unicode(objectType).encode('utf-8'),
+                                 unicode(object).encode('utf-8')))
+        if len(self.pendingRows) >= self.ROWS_PER_QUERY:
+            self._writePendingRows()
+                                
+    def _writePendingRows(self):
+        if len(self.pendingRows) == 0:
+            return
+
+        if self.verbose:
+            print "Inserting %d rows..." % (len(self.pendingRows))
+
+        self.cursor.executemany(
+            """
+            INSERT INTO statements_temp1
+              (subject_text, predicate_text, object_type, object_text)
+            VALUES (%s, %s, %s, %s)""",
+            self.pendingRows)
+        
+        self.pendingRows = []
+
+    def finish(self):
+        self._writePendingRows()
+        
+        writeNum = False;
+        
+        # Create any missing data types.
+        if self.verbose:
+            print "Creating data types..."
+        self.cursor.execute(
+            """
+            INSERT INTO data_types (uri)              
+              SELECT DISTINCT s.object_type
+              FROM statements_temp1 s LEFT JOIN data_types dt 
+                  ON s.object_type = dt.uri
+              WHERE dt.id IS NULL
+            """)        
+       
+        self.cursor.execute(
+            """
+            CREATE TEMPORARY TABLE version_statement_temp1 (
+                version_id integer,
+                stmt_id integer
+            ) ON COMMIT DROP;
+            """)
+                
+        existIDs = []
+        if writeNum:
+            
+            # Create any missing text values
+            if self.verbose:
+                print "Registering subject texts..."
+            self.cursor.execute(
+                """
+                INSERT INTO statement_text (txt)
+                  SELECT DISTINCT s.subject_text
+                  FROM statements_temp1 s 
+                       LEFT JOIN statement_text t ON s.subject_text = t.txt
+                  WHERE t.id IS NULL
+                """)
+            if self.verbose:
+                print "Registering predicate texts..."
+            self.cursor.execute(
+                """
+                INSERT INTO statement_text (txt)
+                  SELECT DISTINCT s.predicate_text
+                  FROM statements_temp1 s 
+                       LEFT JOIN statement_text t ON s.predicate_text = t.txt
+                  WHERE t.id IS NULL
+                """)
+            if self.verbose:
+                print "Registering object texts..."
+            self.cursor.execute(
+                """
+                INSERT INTO statement_text (txt)
+                  SELECT DISTINCT substr(s.object_text, 0, 2700)
+                  FROM statements_temp1 s
+                       LEFT JOIN statement_text t ON substr(s.object_text, 0, 2700) = t.txt
+                  WHERE t.id IS NULL
+                """)
+            
+            # Get IDs of existing statements
+            if self.verbose:
+                print "Checking for existing statements..."
+            self.cursor.execute(
+                """
+                INSERT INTO version_statement_temp1
+                    SELECT %d, ss.id FROM statements_temp1 s
+                      LEFT JOIN statement_text st ON s.subject_text = st.txt
+                      LEFT JOIN statement_text pt ON s.predicate_text = pt.txt
+                      LEFT JOIN statement_text ot ON substr(s.object_text, 0, 2700) = ot.txt
+                      RIGHT JOIN statements ss ON ss.subject = st.id AND ss.predicate = pt.id AND ss.object = ot.id
+                """ % int(self.versionId))
+            
+            # Insert the statements into the statements table.
+            if self.verbose:
+                print "Inserting new statements..."
+            self.cursor.execute(
+                """
+                INSERT INTO statements (subject, predicate, object_type,
+                                        object)
+                 SELECT DISTINCT st.id, pt.id, dt.id, ot.id
+                  FROM statements_temp1 s
+                      LEFT JOIN statement_text st ON s.subject_text = st.txt
+                      LEFT JOIN statement_text pt ON s.predicate_text = pt.txt
+                      LEFT JOIN data_types dt ON s.object_type = dt.uri
+                      LEFT JOIN statement_text ot ON substr(s.object_text, 0, 2700) = ot.txt
+                      LEFT JOIN statements ss ON ss.subject = st.id AND ss.predicate = pt.id AND ss.object = ot.id
+                  WHERE ss.id IS NULL
+                  RETURNING id
+                """)
+            
+            
+        else:
+            
+            # Get IDs of existing statements
+            if self.verbose:
+                print "Checking for existing statements..."
+            self.cursor.execute(
+                """
+                INSERT INTO version_statement_temp1 (version_id, stmt_id)
+                    SELECT %d, ss.id FROM statements_temp1 s
+                      RIGHT JOIN statements ss ON s.subject_text = ss.subject_text AND s.predicate_text = ss.predicate_text AND s.object_text = ss.object_text
+                """ % int(self.versionId)) 
+            
+            # Insert the statements into the statements table.
+            if self.verbose:
+                print "Inserting statements..."
+            self.cursor.execute(
+                """
+                INSERT INTO statements (subject_text, predicate_text, object_type,
+                                        object_text)
+                 SELECT DISTINCT s.subject_text, s.predicate_text, dt.id, s.object_text
+                  FROM statements_temp1 s
+                      LEFT JOIN data_types dt ON s.object_type = dt.uri
+                      LEFT JOIN statements ss
+                          ON ss.subject_text = s.subject_text AND
+                             ss.predicate_text = s.predicate_text AND
+                             ss.object_text = s.object_text
+                  WHERE ss.id IS NULL
+                  RETURNING id
+                """)
+        
+        # Get IDs of newly inserted rows
+        ids = self.cursor.fetchall()
+        ids = [(int(self.versionId), x.pop()) for x in ids]
+        self.cursor.executemany(
+            """
+            INSERT INTO version_statement_temp1 (version_id, stmt_id)
+                VALUES (%d, %d)
+            """, ids)
+        
+        # Add the statement numbers to the version.
+        if self.verbose:
+            print "Saving version information..."        
+        self.cursor.execute(
+            """
+            INSERT INTO version_statement (version_id, stmt_id)
+                SELECT vt.version_id, vt.stmt_id
+                  FROM version_statement_temp1 vt
+                    LEFT JOIN version_statement v ON v.version_id = vt.version_id AND v.stmt_id = vt.stmt_id
+                WHERE v.stmt_id IS NULL
+            """)
+        
+        # Drop temporary table contents
+        self.cursor.execute(
+            """
+            DELETE FROM statements_temp1
+            """)
+        self.cursor.execute(
+            """
+            DROP TABLE version_statement_temp1
+            """)
+        
+    def rollback(self):
+        
+        self.connection.rollback()
+        
+        self.cursor.execute(
+            """
+            CREATE TEMPORARY TABLE statements_temp1 (
+              subject_text text NOT NULL,
+              predicate_text text NOT NULL,
+              object_type text NOT NULL,
+              object_text text NOT NULL
+            ) ON COMMIT DROP;
+            """)
+                
+    def close(self):
+                
+        self.finish()
+
+        self.cursor.close()
+        
+        self.connection.commit()
+        self.connection.close()
+        
+        if self.verbose:
+            print "All done!"
+
+
+_sinkFactories = {
+    'singleversion': SingleVersionRdfSink,
+    }
+
+def getSink(connection, sinkType, **sinkArgs):
+    sinkTypeNorm = sinkType.lower()
+
+    try:
+        return _sinkFactories[sinkTypeNorm](connection, **sinkArgs)
+    except KeyError:
+        raise InstantiationError(_("Invalid sink type '%s'") % sinkType)
+    except TypeError, e:
+        raise InstantiationError(_("Missing or invalid sink arguments: %s") %
+                                 e)
---
+++ relrdf/db/postgres/create-basicschema-v3-text.sql
@@ -0,0 +1,123 @@
+DROP TABLE IF EXISTS relrdf_schema_version;
+
+CREATE TABLE relrdf_schema_version (
+  name varchar(255),
+  version integer
+);
+
+INSERT INTO relrdf_schema_version (name, version)
+  VALUES ('basic', 3);
+
+DROP TABLE IF EXISTS statements;
+
+CREATE TABLE statements (
+  id SERIAL PRIMARY KEY,
+  subject_text text NOT NULL,
+  predicate_text text NOT NULL,
+  object_type integer NOT NULL,
+  object_text text NOT NULL
+);
+
+CREATE INDEX statements_subject_predicate_index
+  ON statements (subject_text, predicate_text);
+  
+DROP TABLE IF EXISTS data_types;
+
+CREATE TABLE data_types (
+  id SERIAL,
+  uri varchar(255) NOT NULL,
+  PRIMARY KEY  (id)
+);
+
+CREATE UNIQUE INDEX data_types_unique
+  ON data_types (uri);
+
+INSERT INTO data_types (id, uri)
+  VALUES (1, 'http://www.w3.org/2000/01/rdf-schema#Resource'), (2, '<BLANKNODE>'), (3, 'http://www.w3.org/2000/01/rdf-schema#Literal');
+
+
+DROP TABLE IF EXISTS version_statement;
+
+CREATE TABLE version_statement (
+  version_id integer NOT NULL,
+  stmt_id integer NOT NULL,
+  PRIMARY KEY (version_id, stmt_id)
+);
+
+CREATE INDEX version_statement_stmt_id_index
+  ON version_statement (stmt_id);
+  
+DROP TABLE IF EXISTS twoway;
+
+CREATE TABLE twoway (
+  version_a integer NOT NULL,
+  version_b integer NOT NULL,
+  stmt_id integer NOT NULL,
+  context char(2) NOT NULL,
+  PRIMARY KEY (version_a, version_b, stmt_id)
+);
+
+
+DROP TABLE IF EXISTS twoway_use_time;
+
+CREATE TABLE twoway_use_time (
+  version_a integer NOT NULL,
+  version_b integer NOT NULL,
+  time timestamp NOT NULL,
+  PRIMARY KEY (version_a, version_b)
+);
+
+
+DROP TABLE IF EXISTS twoway_conns;
+
+CREATE TABLE twoway_conns (
+  version_a integer NOT NULL,
+  version_b integer NOT NULL,
+  connection integer NOT NULL
+);
+
+
+DROP TABLE IF EXISTS threeway;
+
+CREATE TABLE threeway (
+  version_a integer NOT NULL,
+  version_b integer NOT NULL,
+  version_c integer NOT NULL,
+  stmt_id integer NOT NULL,
+  context char(3) NOT NULL,
+  PRIMARY KEY (version_a, version_b, version_c, stmt_id)
+);
+
+
+DROP TABLE IF EXISTS threeway_use_time;
+
+CREATE TABLE threeway_use_time (
+  version_a integer NOT NULL,
+  version_b integer NOT NULL,
+  version_c integer NOT NULL,
+  time timestamp NOT NULL,
+  PRIMARY KEY (version_a, version_b, version_c)
+);
+
+
+DROP TABLE IF EXISTS threeway_conns;
+
+CREATE TABLE threeway_conns (
+  version_a integer NOT NULL,
+  version_b integer NOT NULL,
+  version_c integer NOT NULL,
+  connection integer NOT NULL
+);
+
+DROP TABLE IF EXISTS prefixes;
+
+CREATE TABLE prefixes (
+  prefix varchar(31) NOT NULL PRIMARY KEY,
+  namespace varchar(255) NOT NULL
+);
+
+INSERT INTO prefixes (prefix, namespace)
+  VALUES ('vmxt', 'http://www.v-modell-xt.de/schema/1#'), ('vmxti', 'http://www.v-modell-xt.de/model/1#');
+  
+  
+

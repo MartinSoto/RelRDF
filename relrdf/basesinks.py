06c77b9fcb03e85f49bc48ec7d72e965ed80fed4
Martin Soto <soto@iese.fhg.de>
2009-04-23 18:32:58 +0200

    License under LGPL Version 2
    All source files now have appropriate license headers. A simple README
    for the project was also written.

--- relrdf/basesinks.py
+++ relrdf/basesinks.py
@@ -1,3 +1,27 @@
+# -*- Python -*-
+#
+# This file is part of RelRDF, a library for storage and
+# comparison of RDF models.
+#
+# Copyright (C) 2005-2009, Fraunhofer Institut Experimentelles
+# Software Engineering (IESE).
+#
+# RelRDF is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA. 
+
+
 from relrdf.localization import _
 from relrdf.error import InstantiationError
 from relrdf.expression import uri, blanknode, literal
--- relrdf/results.py
+++ relrdf/results.py
@@ -1,3 +1,27 @@
+# -*- Python -*-
+#
+# This file is part of RelRDF, a library for storage and
+# comparison of RDF models.
+#
+# Copyright (c) 2005-2009 Fraunhofer-Institut fuer Experimentelles
+#                         Software Engineering (IESE).
+#
+# RelRDF is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA. 
+
+
 # Possible result types.
 RESULTS_COLUMNS = 10
 RESULTS_STMTS = 11

9dca544d8a50aa908313ce17a003d8f9899af1aa
Martín Soto <soto@masoft>
2009-05-21 20:00:51 +0200

    Fix user permission settings in create-user.sql
    * relrdf/db/postgres/schema/create-user.sql: readd graph_statement to the
      list of tables.

--- relrdf/db/postgres/schema/create-user.sql
+++ relrdf/db/postgres/schema/create-user.sql
@@ -3,5 +3,5 @@ GRANT ALL ON
   types, data_types_id_seq, language_tags_id_seq, 
   prefixes, relrdf_schema_version,
   statements, statements_id_seq, 
-  graphs, graphs_graph_id_seq/*, graph_statement*/ TO :user;
+  graphs, graphs_graph_id_seq, graph_statement TO :user;
 
